2ca18d7491d7f8b28376c0794b6a8a335adcd21f
Eric Varela <eric_varela@mac.com>
2018-09-23 00:26:35 -0400

    Added Favicon

--- siliconunicorn/blog/urls.py
+++ siliconunicorn/blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.MostRecentEntriesList.as_view(), name='home'),
+    # path('favicon.ico', RedirectView.as_view(
+    #     url=staticfiles_storage.url('blog/favicon/favicon.ico'),), name="favicon"),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('new/', views.NewPost.as_view(), name='new_post'),
     path('signup/', views.SignUp.as_view(), name='signup'),
@@ -16,5 +19,4 @@ urlpatterns = [
     path('ajax/comment/heart', views.ajax_comment_heart, name="heart_comment"),
     path('ajax/post/heart', views.ajax_post_heart, name="heart_post"),
     path('ajax/image/upload', views.ajax_img_upload, name="image_upload"),
-
 ]
